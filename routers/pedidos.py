dc101c0f7bc5424a0a7d68e05f7eafaa1cab34f3
vctg1 <victormourago@gmail.com>
2023-03-31 16:55:55 -0300

    fix pedidos router

--- routers/pedidos.py
+++ routers/pedidos.py
@@ -1,36 +1,23 @@
 from database.tbPedido import TbPedido
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 from fastapi_utils.cbv import cbv
 
 router = APIRouter()
 
 @cbv(router)
-class CorreiosApi:
+class Pedidos:
 
     @router.get("/pedidos")
     def get_pedidos(self):
         return TbPedido.list()
 
     
-    @router.get("/pedido/:id")
-    def get_pedido_by_id(self, id: str):
+    @router.get("/pedido/{codigo}")
+    def get_pedido_by_code(self, id: str = None, codigo_rastreamento: str = None):
+
+        if not(codigo_rastreamento and id):
+            raise HTTPException(status_code=404, detail="Pedido not found")
+        elif(codigo_rastreamento):
+            return TbPedido.get(rastreamento_pedido=codigo_rastreamento)
         
-        res = TbPedido.get(id_pedido=id)
-
-        return res
-    
-
-    @router.get("/pedido/:codigo_rastreamento")
-    def get_pedido_by_rastreamento(self, codigo_rastreamento: str):
-        
-        res = TbPedido.get(rastreamento_pedido=codigo_rastreamento)
-
-        return res
-    
-
-    @router.post("/pedido")
-    def get_pedido_by_id(self, id: str):
-        
-        res = TbPedido.get(id_pedido=id)
-
-        return res
+        return TbPedido.get(id_pedido=id)
